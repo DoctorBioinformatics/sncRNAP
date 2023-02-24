9d84bbd4b29dc2357bb60a519241a3ac4ecb4701
Doctor_Bioinformatics <82383235+DoctorBioinformatics@users.noreply.github.com>
2023-02-24 11:22:52 +0000

    Update get_adapter.py

--- bin/get_adapter.py
+++ bin/get_adapter.py
@@ -3,7 +3,7 @@ import sys
 import gzip
 from concurrent.futures import ProcessPoolExecutor
 
-def get_adapter(fastq,db):
+def get_adapter(fastq, db):
     """A function to identify the most abundant adapter sequence in a fastq file."""
 
     # check if file is gzipped or not
@@ -14,37 +14,32 @@ def get_adapter(fastq,db):
 
     # read fastq file
     with f:
-        file=f.read()
-        query=file.split('@')
-        query=['@'+seq for seq in query][1:]
-
-        reads=[]
-        for i,s in enumerate(query):
-            seq=s.split('\n')[1]
-            seq_flattened_query = ''.join([line.strip() for line in seq])#join lines
-            reads.append(seq_flattened_query)
+        query = []
+        lines = f.readlines()
+        for i in range(0, len(lines), 4):
+            query.append(lines[i + 1].strip())
 
     # read adapter sequence db
     with open(db) as d:
-        database=d.read()
-        database=database.split('>')
-        database=['>'+seq for seq in database[1:]]#join
+        database = d.read()
+        database = database.split('>')
+        database = ['>' + seq for seq in database[1:]]  # join
 
-        adapters=[]
+        adapters = []
         for x in database:
-            database_seq=x.split('\n')[1:]
-            database_seq = ''.join([x.strip() for x in database_seq])#join lines
+            database_seq = x.split('\n')[1:]
+            database_seq = ''.join([x.strip() for x in database_seq])  # join lines
             adapters.append(database_seq)
 
     # match adapter sequence with fastq file
-    match={}
-    for i,s in enumerate(adapters):
-        for j,t in enumerate(reads):
+    match = {}
+    for i, s in enumerate(adapters):
+        for j, t in enumerate(query):
             if s in t:
                 if s not in match:
                     match[s] = 1
                 else:
-                    match[s] +=1
+                    match[s] += 1
 
     # report max counts for adapter sequence
     if len(match) == 0:
@@ -53,8 +48,8 @@ def get_adapter(fastq,db):
     else:
         print(f'{max(match, key=match.get)}')
 
-if __name__== '__main__':
-    fastq=sys.argv[1]
-    db=sys.argv[2]
+if __name__ == '__main__':
+    fastq = sys.argv[1]
+    db = sys.argv[2]
     with ProcessPoolExecutor() as executor:
         executor.submit(get_adapter, fastq, db)
